1a421972c0f5e2f1df36f4e91ab10c432e18435a
Ado Moshe <adoran.moshe@gmail.com>
2018-12-05 13:25:26 -0800

    Finished required functionality with all 4 commands, last reads data from random.txt file

--- liri.js
+++ liri.js
@@ -10,16 +10,21 @@ let command = process.argv[2];
 let input = process.argv[3];
 
 
+console.log('initial input: ' + input);
+console.log('initial command: ' + command);
+
 if (process.argv[4]) {
     input = process.argv.slice(3).join("+");
 };
 
-if (command === 'concert-this') {
+if (command === 'concert-this') { concert(input); };
+
+function concert(concert) {
     axios.get(
-        `https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`
+        `https://rest.bandsintown.com/artists/${concert}/events?app_id=codingbootcamp`
     ).then(function (response) {
         let reply = response.data;
-        console.log(`${input}... performing ${reply.length} events soon!`);
+        console.log(`${concert}... performing ${reply.length} events soon!`);
         for (let i = 0; i < reply.length; i++) {
             console.log(` `);
             console.log(`Venue Name: ${reply[i].venue.name}`);
@@ -31,28 +36,33 @@ if (command === 'concert-this') {
 };
 
 
-if (command === 'spotify-this-song') {
-    if (!input) {input = 'The Sign Ace of Base'}
-   
+if (command === 'spotify-this-song') { song(input); };
+
+function song(song) {
+    if (song === undefined) { song = 'The Sign Ace of Base' }
+
     spotify
-    .search({ type: 'track', query: input, limit:'1' })
-    .then(function(response) {
-        let reply = response.tracks.items[0];
-        console.log('');
-      console.log(`Spotify found "${reply.name}", performed by "${reply.artists[0].name}" from the album "${reply.album.name}"`);
-      console.log(`Spotify link: "${reply.external_urls.spotify}"`);
-      console.log('');
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+        .search({ type: 'track', query: song, limit: '1' })
+        .then(function (response) {
+            let reply = response.tracks.items[0];
+            console.log('');
+            console.log(`Spotify found "${reply.name}", performed by "${reply.artists[0].name}" from the album "${reply.album.name}"`);
+            console.log('');
+            console.log(`Spotify link: "${reply.external_urls.spotify}"`);
+            console.log('');
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 };
 
 
-if (command === 'movie-this') {
-    if (!input) {input = 'Mr. Nobody'}
+if (command === 'movie-this') { movie(input); };
+
+function movie(movie) {
+    if (movie === undefined) { movie = 'Mr. Nobody' };
     axios.get(
-        "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy"
+        `http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=trilogy`
     ).then(function (response) {
         let reply = response.data
         console.log('');
@@ -68,3 +78,14 @@ if (command === 'movie-this') {
 };
 
 
+if (command === 'do-what-it-says') {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = data.split(',');
+
+        if (data[0] === 'spotify-this-song');
+        song(data[1]);
+    });
+};
